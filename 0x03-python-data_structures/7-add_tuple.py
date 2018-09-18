06002442caa5a285590d623b7d05625af632dacc
dmurr <diegocmurray@gmail.com>
2018-09-18 23:34:16 +0000

    Adds corresponding elements from two tuples together

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    if len(tuple_a) is 0:
+        i = tuple_b[0]
+        j = tuple_b[1]
+    elif len(tuple_b) is 0:
+        i = tuple_a[0]
+        j = tuple_a[1]
+    elif len(tuple_a) is 1 :
+        i = tuple_a[0]
+        j = tuple_a[1] + tuple_b[1]
+    elif len(tuple_b) is 1 :
+        i = tuple_a[0] + tuple_b[0]
+        j = tuple_a[1]
+    else:
+        i = tuple_a[0] + tuple_b[0]
+        j = tuple_a[1] + tuple_b[1]
+    return (i,j)
