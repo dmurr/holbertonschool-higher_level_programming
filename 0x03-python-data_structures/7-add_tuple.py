8bf529054598cb82c224685117135fc6895470aa
dmurr <diegocmurray@gmail.com>
2018-09-19 01:06:28 +0000

    Switched approach to problem. Switch tuple to a list and append 0's as needed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) is 0 and len(tuple_b) is 0:
-        return (0,0)
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+    len_a = len(list_a)
+    len_b = len(list_b)
 
-    if len(tuple_a) is 0:
-        i = tuple_b[0]
-        j = tuple_b[1]
-    elif len(tuple_b) is 0:
-        i = tuple_a[0]
-        j = tuple_a[1]
-    elif len(tuple_a) is 1 :
-        i = tuple_a[0]
-        j = tuple_a[1] + tuple_b[1]
-    elif len(tuple_b) is 1 :
-        i = tuple_a[0] + tuple_b[0]
-        j = tuple_a[1]
-    else:
-        i = tuple_a[0] + tuple_b[0]
-        j = tuple_a[1] + tuple_b[1]
-    return (i,j)
+    while len(list_a) < 2:
+        list_a.append(0)
+        len_a += 1
+    while len(list_b) < 2:
+        list_b.append(0)
+        len_b += 1
+    x = list_a[0] + list_b[0]
+    y = list_a[1] + list_b[1]
+    return x, y
