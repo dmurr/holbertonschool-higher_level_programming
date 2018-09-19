eecfd4ec0943a940bc1cfd701f90aacfd4cc942d
dmurr <diegocmurray@gmail.com>
2018-09-19 03:22:40 +0000

    Changed for loop condition

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if len(my_list) is 0:
         return None
     new_list = []
-    for i in range(len(my_list)):
+    for i in my_list:
         if i % 2 is 0:
             new_list.append(True)
         else:
