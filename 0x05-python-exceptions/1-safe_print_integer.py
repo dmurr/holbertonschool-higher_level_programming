39cb3112ef7cd5c9dc3af7352c1eb968464af97d
dmurr <diegocmurray@gmail.com>
2018-10-01 19:03:09 +0000

    Trys to print integer and checks for exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    printed = False
+    try:
+        print("{:d}".format(value))
+        printed = True
+    except:
+        printed = False
+    return printed
