755f18688ed1cc199c8d8b12b01930571143356b
dmurr <diegocmurray@gmail.com>
2018-10-02 01:24:20 +0000

    Nested everything within Try/Except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            count += 1
+    try:
+        count = 0
+        for i in range(x):
             print(my_list[i], end="")
-        except:
-            pass
+            count += 1
+    except:
+        pass
     print("")
     return count
