e21a182ee0f3d663a206a8b2926f9b219cfc113c
dmurr <diegocmurray@gmail.com>
2018-10-06 03:43:19 +0000

    function that adds 2 integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+xc#!/usr/bin/python3
+"""
+This module contains an addition function.
+
+
+"""
+def add_integer(a, b=98):
+    """ Addition Function """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
