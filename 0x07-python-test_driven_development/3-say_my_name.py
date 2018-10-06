cc3f56b2dd2566a20a13bb9900b8ae27776613db
dmurr <diegocmurray@gmail.com>
2018-10-06 03:45:11 +0000

    function that prints My name is <first name> <last name>

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module contains say_my_name function
+
+"""
+def say_my_name(first_name, last_name=""):
+    """ Function prints greeting with first and last name """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
