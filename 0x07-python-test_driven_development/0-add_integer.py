e193e06c92ea3945d744f9b0d629423f459ca9ba
dmurr <diegocmurray@gmail.com>
2018-10-08 01:57:13 +0000

    Added checks for for float overflow after addition and Nan

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,14 +5,19 @@ This module contains an addition function.
 
 
 def add_integer(a, b=98):
-    """
-    Addition Function
-    """
+    """ Addition Function """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    if a != a or b != b:
+        raise ValueError("unsupported type")
+
+    result = a + b
+    if result == float('inf') or result == float('inf'):
+        raise ValueError("Error")
 
     return int(a) + int(b)
