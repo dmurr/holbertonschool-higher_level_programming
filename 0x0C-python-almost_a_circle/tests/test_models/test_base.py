b87164075a2b5827880921c331439bd8fb68300c
dmurr <diegocmurray@gmail.com>
2018-10-23 03:21:23 +0000

    testing bass class id

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,10 +3,20 @@
 Module contains unittests for file base.py
 """
 import unittest
-from models import Base
+from models.base import Base
 
 
 class TestBase(unittest.TestCase):
 
-    def test_method(self):
-        #include many edge cases in here
+    def test_id(self):
+        x1 = Base()
+        self.assertEqual(x1.id, 1)
+        self.assertEqual(x1.id, Base._Base__nb_objects)
+        x2 = Base()
+        self.assertEqual(x2.id, 2)
+        self.assertEqual(x2.id, Base._Base__nb_objects)
+        x3 = Base(44)
+        self.assertEqual(x3.id, 44)
+        x4 = Base()
+        self.assertEqual(x4.id, 3)
+        self.assertEqual(x4.id, Base._Base__nb_objects)
