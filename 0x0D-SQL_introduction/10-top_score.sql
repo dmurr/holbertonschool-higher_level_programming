4025b589a96d898a6b50cea1f92aebd0e67182d5
dmurr <diegocmurray@gmail.com>
2018-12-04 04:09:19 +0000

    Lists all records sorts by score in descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Lists all records of the table second_table of the database hbtn_0c_0
+-- Results display both the score and the name
+-- Records are be ordered by score top first
+-- The database name will be passed as an argument of the mysql command
+SELECT *
+       FROM second_table
+       ORDER BY score DESC;
