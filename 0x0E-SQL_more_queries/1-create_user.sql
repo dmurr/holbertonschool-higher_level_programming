f59bb1c81271b250db5b3eec9cf8741859b1a7dd
dmurr <diegocmurray@gmail.com>
2018-12-04 22:27:14 +0000

    Removed 'WITH GRANT OPTION'

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- Sets pass to user_0d_1_pwd and grants all privileges
 -- Does not fail is user already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
