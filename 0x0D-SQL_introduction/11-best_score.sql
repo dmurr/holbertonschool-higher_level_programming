4a3423e04aef1f15868dac7e191ec420ab09ea09
dmurr <diegocmurray@gmail.com>
2018-12-04 04:17:33 +0000

    Added mandatory FROM

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,6 @@
 -- Lists all records with a score >= 10 in the table second_table
 -- The database name will be passed as an argument of the mysql
 SELECT score, name
+       FROM second_table
        WHERE score >= 10
        ORDER BY score DESC;
