6efef7b3908dc62e08707c2d8a573e9c5049da51
dmurr <diegocmurray@gmail.com>
2018-12-04 04:23:33 +0000

    Update score attibute to new value where a condition is met

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Updates the score of Bob to 10 in the table second_table
+-- The database name will be passed as an argument of the mysql
+UPDATE `second_table`
+SET `score` = 10;
+WHERE `name` = "BOB";
