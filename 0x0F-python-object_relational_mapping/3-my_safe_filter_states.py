79e243629492e9299a62abf29519526196addbc6
dmurr <diegocmurray@gmail.com>
2019-01-04 03:46:27 +0000

    Fixed: PEP8 Styling

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY states.id",
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s "
+                "ORDER BY states.id",
                 (argv[4],))
     rows = cur.fetchall()
     for row in rows:
