70d4480ba755ba05889bd6456da945607b9bca8c
dmurr <diegocmurray@gmail.com>
2019-01-04 03:36:28 +0000

    initial commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" script that lists all states whose name starts with N """
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+            print ("{}".format(row))
+    cur.close()
+    dc.close()
