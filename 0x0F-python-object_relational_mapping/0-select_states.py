4a6dfc47e5d77f310efe95659075270a81b80396
dmurr <diegocmurray@gmail.com>
2019-03-30 05:08:07 +0000

    fixed shebang

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ Lists all states from the database hbtn_0e_0_usa
 """
 
-import MySQLdb
-from sys import argv
-
 if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
