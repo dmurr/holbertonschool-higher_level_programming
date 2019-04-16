f8eacca3f48aff1a73f97933796eefee57c51183
dmurr <diegocmurray@gmail.com>
2019-04-16 01:54:56 +0000

    Sends get request to url and raises HTTP errors

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Sends request to url and manages errors
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    r = requests.get(argv[1])
+
+    if r.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
+    else:
+        print(r.text)
