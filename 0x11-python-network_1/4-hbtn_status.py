46e9c3e243a379cf1a43c91ad609132b20a6ac35
dmurr <diegocmurray@gmail.com>
2019-04-16 04:40:26 +0000

    reformatted output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import requests
 
     r = requests.get('https://intranet.hbtn.io/status')
-    print('''Body response:
-    - type: {}
-    - content: {}'''.format(type(r.text), r.text))
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
